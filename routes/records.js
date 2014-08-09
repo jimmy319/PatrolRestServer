8ae5be952aa1f2e56c4933cd2efa8b0052553bf1
Jimmy Tseng <jimmy74319@gmail.com>
2014-08-09 17:30:37 +0800

    correct timestamp format; remove mac and userID fields from records list view

--- routes/records.js
+++ routes/records.js
@@ -21,22 +21,29 @@ router.post('/',function(req, res){
 				res.send({status:'fail',log:'user info error'});		
 			}else{
 				userData = user;
-
-				//fetch spot info
-				db.collection('spots').find({'uid':req.body.uid}).toArray(function(err, spot){
-					if(err){
-						res.send({status:'fail',log:'spot info error'});		
-					}else{
-						spotData = spot;
-						
-						//insert data to records collection
-						db.collection('records').insert({timestamp:new Date().toDateString(),mac:spotData[0].mac,spotName:spotData[0].name,userName:userData.name,userId:userData._id.toString()},function(err,result){
-							res.send(
-								(err===null) ? {status:'success'} : {status:'fail'}
-							);
-						});
-					}
-				});
+				if(userData){
+					//fetch spot info
+					db.collection('spots').find({'uid':req.body.uid}).toArray(function(err, spot){
+						if(err){
+							res.send({status:'fail',log:'spot info error'});		
+						}else{
+							spotData = spot;
+							
+							//insert data to records collection
+							if(spotData&&spotData.length>0){
+								db.collection('records').insert({timestamp:new Date().toString().replace(/GMT\+.*?\)$/,""),mac:spotData[0].mac,spotName:spotData[0].name,userName:userData.name,userId:userData._id.toString()},function(err,result){
+									res.send(
+										(err===null) ? {status:'success'} : {status:'fail'}
+									);
+								});
+							}else{
+								res.send({status:'fail',log:'spot info error'});
+							}
+						}
+					});
+				}else{
+					res.send({status:'fail',log:'user info error'});
+				}
 			}
 		});
 	}
